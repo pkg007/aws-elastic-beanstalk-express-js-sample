8a90506d68d32b899c059d4f890e82e1dfcf74e2
Pkg <pramod.x.gupta@gmail.com>
2021-09-29 22:54:11 +1300

    Full DevOps pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello to Pramod World!'));
+app.get('/', (req, res) => res.send('Hello to Pramod World- Test full devOps pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
